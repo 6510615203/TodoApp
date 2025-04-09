98b3745a2337856ca50ae718c860fa57ba7cc5df
Ponthipa <6510615203@student.tu.ac.th>
2025-04-09 15:21:19 +0700

    In class

--- TodoApp/Views/RegisterView.swift
+++ TodoApp/Views/RegisterView.swift
@@ -9,22 +9,23 @@ import SwiftUI
 
 struct RegisterView: View {
     @StateObject var viewModel = RegisterViewModel()
+    
     var body: some View {
         VStack {
             HeaderView(title: "Register", subtitle: "Start organizing todos", background: .orange)
-        }
-        Form {
-            TextField("Full Name", text: $viewModel.name)
-                .textFieldStyle(DefaultTextFieldStyle())
-            TextField("Email Address", text: $viewModel.email)
-                .textFieldStyle(DefaultTextFieldStyle())
-                .autocorrectionDisabled()
-                .autocapitalization(.none)
-            SecureField("Password", text: $viewModel.password)
-                .textFieldStyle(DefaultTextFieldStyle())
-            
-            TLButton(title: "Create Account", background: .green) {
-                viewModel.register()
+            Form {
+                TextField("Full Name", text: $viewModel.name)
+                    .textFieldStyle(DefaultTextFieldStyle())
+                TextField("Email Address", text: $viewModel.email)
+                    .textFieldStyle(DefaultTextFieldStyle())
+                    .autocorrectionDisabled()
+                    .autocapitalization(.none)
+                SecureField("Password", text: $viewModel.password)
+                    .textFieldStyle(DefaultTextFieldStyle())
+                
+                TLButton(title: "Create Account", background: .green) {
+                    viewModel.register()
+                }
             }
         }
     }
--- TodoApp/Views/TLButton.swift
+++ TodoApp/Views/TLButton.swift
@@ -11,13 +11,14 @@ struct TLButton: View {
     let title: String
     let background: Color
     let action: () -> Void
+    
     var body: some View {
         Button {
             action()
         } label: {
             ZStack {
                 RoundedRectangle(cornerRadius: 10)
-                    .foregroundColor(.blue)
+                    .foregroundColor(background)
                 Text(title)
                     .foregroundColor(.white)
                     .bold()
